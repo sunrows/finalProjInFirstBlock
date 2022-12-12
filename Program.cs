8bffc1dcba4c70e7da303c543859918cdb14ff13
sunrowsforus <sunrowsforus@mail.ru>
2022-12-13 00:16:24 +0300

    Add some new rows in our code

--- Program.cs
+++ Program.cs
@@ -1,19 +1,21 @@
-﻿void myArray(string [] arr)
+﻿void myArray(string[] arr)
 {
     //string[] array = new string[arr.Length];
     string[] myArray = new string[arr.Length];
+    Console.Write("myArray = [");
     for (int i = 0; i < arr.Length; i++)
     {
-        if (arr[i].Length<=3) 
+        if (arr[i].Length <= 3)
         {
             myArray[i] = arr[i];
+            //Console.Write(string.Join(", ", (myArray[i]))); - Это почему то не работает!
+            Console.Write(myArray[i] + " ");
         }
-    Console.WriteLine(myArray[i]);  
     }
-
+    Console.Write("]");
 }
 
-string [] inputArray = {"123","Heelo","456","abc","weddings"};
+string[] inputArray = { "123", "Heelo", "456", "abc", "weddings" };
 
 myArray(inputArray);
 
