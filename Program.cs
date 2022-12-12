e39d1857d194b43fe76a2f5f6fa515c3bae70ffd
sunrowsforus <sunrowsforus@mail.ru>
2022-12-12 23:58:01 +0300

    We solve the problem and add some new features on tasks

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void myArray(string [] arr)
+{
+    //string[] array = new string[arr.Length];
+    string[] myArray = new string[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length<=3) 
+        {
+            myArray[i] = arr[i];
+        }
+    Console.WriteLine(myArray[i]);  
+    }
+
+}
+
+string [] inputArray = {"123","Heelo","456","abc","weddings"};
+
+myArray(inputArray);
+
+
